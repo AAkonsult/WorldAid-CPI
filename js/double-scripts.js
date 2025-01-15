93757cc771666d5f951ee597df82b1433a1bda4a
XshiroeXsoraX <johnbook123@live.com>
2025-01-15 16:56:04 +1100

    updated payment links for giving day and virtual seat pages, started thank you page

--- js/double-scripts.js
+++ js/double-scripts.js
@@ -19,7 +19,7 @@ function validateEmail(string) {
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(), //Change year value if needed
         nextYear = yyyy + 1, 
-        dayMonth = "1/11/", //Change date if needed (month/day)
+        dayMonth = "1/18/", //Change date if needed (month/day)
         deadline = dayMonth + yyyy;
     
     today = mm + "/" + dd + "/" + yyyy;
